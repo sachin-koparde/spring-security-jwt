8e7d700861349f355da4d2882a0ec3cd66f00f84
Sachin Koparde <sachinkoparde26@gmal.com>
2021-06-14 20:37:18 +0530

    JWTUtil is updated

---
+++ src/main/java/io/springsecurity/springsecurityjwt/models/AuthenticationRequest.java
@@ -0,0 +1,4 @@
+package io.springsecurity.springsecurityjwt.models;
+
+public class AuthenticationRequest {
+}
--- src/main/java/io/springsecurity/springsecurityjwt/util/JwtUtil.java
+++ src/main/java/io/springsecurity/springsecurityjwt/util/JwtUtil.java
@@ -2,8 +2,12 @@ package io.springsecurity.springsecurityjwt.util;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.springsecurity.springsecurityjwt.user.User;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 public class JwtUtil {
@@ -26,5 +30,26 @@ public class JwtUtil {
     private Claims extractAllClaims(String token) {
         return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
     }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public String generateToken(User user) {
+        Map<String, String> claims = new HashMap<>();
+        return createToken(claims, user.getUsername());
+    }
+
+    private String createToken(Map<String, String> claims, String subject) {
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+    }
+
+    public Boolean validateToken(String token, User user) {
+        final String username = extractUserName(token);
+        return (username.equals(user.getUsername()) && !isTokenExpired(token));
+    }
+
 
 }
